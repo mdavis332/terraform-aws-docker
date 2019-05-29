e96adcb0560d5ea47eb289030b28d2b0ca4db3ce
Michael Davis <mdavis332@gmail.com>
2019-05-29 07:52:40 -0500

    sudo doesn't apply to redirect. change to tee.

--- setup-scripts/dns-change.sh
+++ setup-scripts/dns-change.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-sudo echo "localhost" > /etc/hostname
+echo 'localhost' | sudo tee /etc/hostname
+# remove resolv.conf first since it's usually a symlink rather than a file
 sudo rm /etc/resolv.conf
-sudo echo "nameserver 1.1.1.1" > /etc/resolv.conf
+echo 'nameserver 1.1.1.1' | sudo tee /etc/resolv.conf
 
 sudo systemctl disable systemd-resolved
 sudo systemctl stop systemd-resolved
